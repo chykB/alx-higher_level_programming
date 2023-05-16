2a9953ee28d1a10d7b0ae49a66eb47442f30759b
chykB <malikchika86@gmail.com>
2023-05-16 14:51:56 -0700

    insert value into first-table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert a new row into the first-table.
-INSERT INTO `firt_table` (`id`, `name`) VALUES (89, "Best School"))
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School"))
 
