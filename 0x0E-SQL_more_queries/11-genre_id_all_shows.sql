ab49f13af467ba91b46e18790dab4995a5d60cc3
chykB <malikchika86@gmail.com>
2023-05-18 13:17:56 -0700

    a script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,7 +3,7 @@
 -- If a show doesn’t have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre.id
 FROM tv_shows
-	LEFT JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.titles, tv_show_genres.genre_id;
 
