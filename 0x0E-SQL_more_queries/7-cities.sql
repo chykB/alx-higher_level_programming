9545b3dfe5f5e51fb91603df88079cef528983fe
chykB <malikchika86@gmail.com>
2023-05-18 10:03:41 -0700

    create a database and a table including attributes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- this script  creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	PRIMARY KEY(id),
+	id INT AUTO_INCREMENT  NOT NULL,
 	state_id INT NOT NULL,
 	FOREIGN KEY(state_id),
 	REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL);
+
