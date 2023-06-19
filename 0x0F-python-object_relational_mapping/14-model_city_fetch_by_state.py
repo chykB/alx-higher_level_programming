efdcc3da83acde326fbc0e27ce3ef9b1ace4a80c
chykB <malikchika86@gmail.com>
2023-06-18 20:08:10 -0700

    print  all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" a script that changes the name of a State object from the database
+""" print  all City objects from the database 
 """
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -13,6 +14,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
-    session.commit()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
